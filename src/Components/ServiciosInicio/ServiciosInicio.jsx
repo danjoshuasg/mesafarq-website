658cc615b4340c683a986ba83a475ce91d54b086
Dan Joshua Santivañez Gutarra <83432705+djsg2021utec@users.noreply.github.com>
2024-03-14 00:33:00 -0500

    Cambios 14.03.24

--- src/Components/ServiciosInicio/ServiciosInicio.jsx
+++ src/Components/ServiciosInicio/ServiciosInicio.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import './ServiciosInicio.css';
+import cadena_logistica_img from '../Assets/Servicios/cadena logistica.png';
+import consultoria_img from '../Assets/Servicios/consultoria.png'
 
 const ServiciosInicio = () => {
   const servicios = [
     {
       nombre: 'Gestión de la cadena de suministros',
       descripcion: 'Ofrecemos eficiencia y eficacia en todos los procesos.',
-      imagen: 'ruta-a-la-imagen-de-cadena-de-suministros.jpg'
+      imagen: cadena_logistica_img
     },
     {
       nombre: 'Consultoría en regulación farmacéutica',
       descripcion: 'Ofrecemos la guía directa de expertos en regulación de medicamentos.',
-      imagen: 'ruta-a-la-imagen-de-consultoria.jpg'
+      imagen: consultoria_img
     },
     // Puedes añadir más servicios aquí
   ];
 
   return (
     <div className="servicios">
+      <h2>SERVICIOS QUE OFRECEMOS</h2>
       {servicios.map((servicio, index) => (
         <div key={index} className="servicio">
           <div className="servicio-imagen">
@@ -25,7 +28,9 @@ const ServiciosInicio = () => {
           </div>
           <div className="servicio-info">
             <h3>{servicio.nombre}</h3>
-            <p>{servicio.descripcion}</p>
+            <div className="contenedor-descripcion">
+              <p>{servicio.descripcion}</p>
+            </div>
             <button className="leer-mas">Leer más</button>
           </div>
         </div>
